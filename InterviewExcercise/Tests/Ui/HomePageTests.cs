e47944566b7b5c63a87ab3ffc771583343c287b2
Manuel Marcatili <marca.manu2@gmail.com>
2021-10-27 10:43:22 +0200

    Added selenium integration plus a test on the home page

---
+++ InterviewExcercise/Tests/Ui/HomePageTests.cs
@@ -0,0 +1,56 @@
+﻿using InterviewExcercise.Reporter;
+using InterviewExcercise.UiClient.Pages;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace InterviewExcercise.Tests
+{
+    public class HomePageTests
+    {
+        public void CloseAll()
+        {
+            ReportFixture.Instance.Close();
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+            var stacktrace = TestContext.CurrentContext.Result.StackTrace;
+            var errorMessage = "<pre>" + TestContext.CurrentContext.Result.Message + "</pre>";
+            switch (status)
+            {
+                case TestStatus.Failed:
+                    ReportFixture.Instance.SetTestStatusFail($"<br>{errorMessage}<br>Stack Trace: <br>{stacktrace}<br>");
+                    break;
+                case TestStatus.Skipped:
+                    ReportFixture.Instance.SetTestStatusSkipped();
+                    break;
+                default:
+                    ReportFixture.Instance.SetTestStatusPass();
+                    break;
+            }
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            ReportFixture.Instance.CreateTest(TestContext.CurrentContext.Test.Name);
+        }
+
+        [Test]
+        public void VerifyElementsInLandingPage()
+        {
+            using (var driver = new ChromeDriver())
+            {
+                var homePage = new HomePage(driver);
+                homePage.Load();
+
+                Assert.True(homePage.IsLogoDisplayed());
+                ReportFixture.Instance.SetStepStatusPass("Logo is correctly displayed");
+            }
+        }
+    }
+}
---
+++ InterviewExcercise/UiClient/Pages/HomePage.cs
@@ -0,0 +1,30 @@
+﻿using InterviewExcercise.Reporter;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace InterviewExcercise.UiClient.Pages
+{
+    class HomePage
+    {
+        private IWebDriver Driver;
+
+        public HomePage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        IWebElement logo => Driver.FindElement(By.CssSelector("a[class='logo'] img[alt='Coingaming']:not([class='logo-for-mobile'])"));
+
+        public void Load()
+        {
+            Driver.Navigate().GoToUrl("https://yolo.group/");
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+
+            wait.Until(condition => logo.Displayed);
+            ReportFixture.Instance.SetStepStatusPass("Navigated to Yolo Group home page: https://yolo.group/" );
+        }
+
+        public bool IsLogoDisplayed() => logo.Displayed;
+    }
+}
